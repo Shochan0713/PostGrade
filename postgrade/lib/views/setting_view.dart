c4b09ce93be69b8b107ebb3958a5b83a32af2ead
SHOCHAN <96648577+Shochan0713@users.noreply.github.com>
2024-10-20 23:48:54 +0900

    設定画面の修正

--- postgrade/lib/views/setting_view.dart
+++ postgrade/lib/views/setting_view.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
 import 'package:postgrade/styles/custom_app_bar.dart';
 
@@ -17,21 +16,30 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   final TextEditingController _userNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
+  String profileImageUrl =
+      'https://via.placeholder.com/150'; // デフォルトのプロフィール画像URL
 
   @override
   void initState() {
     super.initState();
-    String? userId = FirebaseAuth.instance.currentUser?.uid; // 現在のユーザーIDを取得
-    _loadUserData(userId!);
+    _loadUserData();
   }
 
-  Future<void> _loadUserData(String userId) async {
-    DocumentSnapshot userDoc =
-        await FirebaseFirestore.instance.collection('users').doc(userId).get();
+  Future<void> _loadUserData() async {
+    final userId = FirebaseAuth.instance.currentUser?.uid; // 現在のユーザーIDを取得
 
-    if (userDoc.exists) {
-      _userNameController.text = userDoc['userName'] ?? '';
-      _emailController.text = userDoc['email'] ?? '';
+    if (userId != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .get();
+      if (userDoc.exists) {
+        _userNameController.text = userDoc['userName'] ?? '';
+        _emailController.text = userDoc['email'] ?? '';
+
+        // プロフィール画像のURLを取得
+        profileImageUrl = userDoc['profileImageUrl'] ?? profileImageUrl;
+      }
     }
   }
 
@@ -44,35 +52,53 @@ class _SettingPageState extends State<SettingPage> {
       'email': _emailController.text.trim(),
     });
 
-    GoRouter.of(context).go('/'); // ログインページにリダイレクト
+    GoRouter.of(context).go('/'); // ホームページにリダイレクト
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        showBackButton: true,
-      ),
+      appBar: CustomAppBar(showBackButton: true),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _userNameController,
-              decoration: const InputDecoration(labelText: 'ユーザーネーム'),
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'メールアドレス'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _saveProfileSettings,
-              child: const Text('保存'),
-            ),
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildProfileImage(),
+              const SizedBox(height: 16),
+              _buildTextField(_userNameController, 'ユーザーネーム'),
+              _buildTextField(_emailController, 'メールアドレス'),
+              const SizedBox(height: 20),
+              _buildSaveButton(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildProfileImage() {
+    // プロフィール画像の表示
+    return CircleAvatar(
+      radius: 50,
+      backgroundImage: NetworkImage(profileImageUrl),
+    );
+  }
+
+  Widget _buildTextField(TextEditingController controller, String label) {
+    // テキストフィールドのビルダー
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(labelText: label),
+    );
+  }
+
+  Widget _buildSaveButton() {
+    // 保存ボタンのビルダー
+    return ElevatedButton(
+      onPressed: _saveProfileSettings,
+      child: const Text('保存'),
     );
   }
 }
